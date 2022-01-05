cde91b11fef55853c5bee23ca54c3dd18c3d3332
Robert Gaspar <gaspar.robert7@gmail.com>
2022-01-05 13:04:14 -0600

    commit tree 903c949538ce3e524404ea1e408f5e5bc2f72461

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10) #
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: { reviews: { include: :user } })
   end
 
 end
