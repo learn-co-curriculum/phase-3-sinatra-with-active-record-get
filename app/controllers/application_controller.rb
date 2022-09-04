6a3d411a8d6c04d1f157d098825e9bf2086a7e25
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-04 08:00:21 +0300

    commit tree 2e6a64bf57ff17757051ed7e90c43c400d4b134c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
+
 
 end
